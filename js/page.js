09a8a22734378c11b924977e33f77cafaa12c857
wjdgodnd3@naver.com <wjdgodnd3@naver.com>
2023-11-22 17:43:19 +0900

    최종11-21

--- js/page.js
+++ js/page.js
@@ -14,38 +14,45 @@ function collapse(element) {
     }
 }
 
-var canvas = document.getElementById("pie-chart");
-var ctx = canvas.getContext("2d");
-
-// Function to draw a pie chart segment
-function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color) {
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(centerX, centerY);
-  ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-  ctx.closePath();
-  ctx.fill();
-}
-
-// Data for the pie chart
-var data = [38, 23, 17, 13, 9]; // Values for each segment (total should be 100%)
-
-// Draw the pie chart
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-var radius = Math.min(canvas.width, canvas.height) / 2;
-
-var totalValue = data.reduce(function (a, b) { return a + b; }, 0);
-var startAngle = 0;
-
-// Colors for each segment
-var colors = ["#FF5733", "#33FF57", "#3357FF", "#FF3357", "#57FF33"];
-
-for (var i = 0; i < data.length; i++) {
-  var sliceAngle = (2 * Math.PI * data[i]) / totalValue;
-  var endAngle = startAngle + sliceAngle;
-
-  drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, colors[i]);
-
-  startAngle = endAngle;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    // 첫 번째 파이 차트를 그리는 함수 호출
+    drawPieChart("pie-chart1", [38, 23, 17, 13, 9], ["#C3ABFF33", "#FFC4D733", "#FFF38933", "#D5FF9E33", "#C6DAFF33"]);
+  
+    // 두 번째 파이 차트를 그리는 함수 호출
+    drawPieChart("pie-chart2", [44, 19, 18, 13, 6], ["#C3ABFF33", "#FFC4D733", "#FFF38933", "#D5FF9E33", "#C6DAFF33"]);
+  });
+  
+  // 파이 차트를 그리는 함수
+  function drawPieChart(canvasId, data, colors) {
+    var canvas = document.getElementById(canvasId);
+  
+    if (canvas) {
+      var ctx = canvas.getContext("2d");
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2;
+      var radius = Math.min(canvas.width, canvas.height) / 2;
+      var totalValue = data.reduce(function (a, b) { return a + b; }, 0);
+      var startAngle = 0;
+  
+      for (var i = 0; i < data.length; i++) {
+        var sliceAngle = (2 * Math.PI * data[i]) / totalValue;
+        var endAngle = startAngle + sliceAngle;
+  
+        drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, colors[i]);
+  
+        startAngle = endAngle;
+      }
+    } else {
+      console.error("Canvas element not found");
+    }
+  }
+  
+  // 그림을 그리는 함수
+  function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath();
+    ctx.fill();
+  }
