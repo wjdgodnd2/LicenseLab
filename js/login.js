e7ba62d3997a428b10e131f04cc63d338e28bb81
rladmdtn <rladmdtn0209@naver.com>
2023-11-22 22:21:01 +0900

    eungsu2_2023-11-22

--- js/login.js
+++ js/login.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
       // 폼에서 값 가져오기
       const username = document.getElementById("username-field").value;
       const password = document.getElementById("password-field").value;
-  
+      const loginLabel = document.getElementById("login-label");
+
       // 사용자 인증 확인 (실제 환경에서는 서버에서 수행될 것입니다)
       if (username === "admin" && password === "admin") {
         // 리다이렉트 또는 성공 메시지 표시
         alert("로그인 성공!"); // 이 부분을 선호하는 성공 표시 방법으로 대체할 수 있습니다.
         
         window.location.href = "./LicenseLab/index.html";
-
         loginLabel.innerText = "Logout";
       } else {
         alert("아이디 혹은 비밀번호를 잘 못 누르셨습니다.");
